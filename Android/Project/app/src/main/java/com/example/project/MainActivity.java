0a3a1f30d021bd07a56fa83a59a4b83efed52604
Quyet Sinh <67093870+sinhnguyen2702@users.noreply.github.com>
2021-04-15 23:50:54 +0700

    create project

---
+++ Android/Project/app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+    }
+}
