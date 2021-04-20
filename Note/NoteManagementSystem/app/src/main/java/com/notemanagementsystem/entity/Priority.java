cc55ad477d58e8960fe52c8bc7f75b6e1c04b4dc
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2021-04-20 17:44:30 +0700

    Add Entity

--- Note/NoteManagementSystem/app/src/main/java/com/notemanagementsystem/entity/Priority.java
+++ Note/NoteManagementSystem/app/src/main/java/com/notemanagementsystem/entity/Priority.java
@@ -1,14 +1,13 @@
 package com.notemanagementsystem.entity;
 
-
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import java.util.Date;
 
-@Entity(tableName = "category")
-public class Category {
+@Entity(tableName = "priority")
+public class Priority {
     @PrimaryKey(autoGenerate = true)
     public int uid;
 
---
+++ Note/NoteManagementSystem/app/src/main/java/com/notemanagementsystem/entity/Status.java
@@ -0,0 +1,4 @@
+package com.notemanagementsystem.entity;
+
+public class Status {
+}
