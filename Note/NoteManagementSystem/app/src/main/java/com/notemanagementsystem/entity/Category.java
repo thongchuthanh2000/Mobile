5390756957c0112c90802a4bf4cbdd7a67040c5b
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2021-04-20 17:20:43 +0700

    Update gradle.xml

---
+++ Note/NoteManagementSystem/app/src/main/java/com/notemanagementsystem/entity/Category.java
@@ -0,0 +1,22 @@
+package com.notemanagementsystem.entity;
+
+import android.text.format.DateUtils;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity
+public class Category {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "createdDate")
+    public Date createdDate;
+
+}
