715900697a7a5c8978d5049b72a3b10581ba28d7
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2021-04-23 09:53:04 +0700

    Home

--- Note/NoteManagementSystem/app/src/main/java/com/notemanagementsystem/fragment/HomeFragment.java
+++ Note/NoteManagementSystem/app/src/main/java/com/notemanagementsystem/fragment/HomeFragment.java
@@ -2,27 +2,61 @@ package com.notemanagementsystem.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 import com.notemanagementsystem.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
 
+    private AnyChartView anyChartView;
     public HomeFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        anyChartView = view.findViewById(R.id.any_chart_view);
+        setupPieChart();
+        return  view;
     }
+
+    private  void setupPieChart(){
+        String [] status = {"Done","Pending","Processing"};
+        int [] value = {1,2,3};
+        List<DataEntry> dataEntries = new ArrayList<>();
+
+        Pie pie = AnyChart.pie();
+        for (int i=0;i<status.length;i++){
+            dataEntries.add(new ValueDataEntry(status[i],value[i]));
+        }
+        pie.data(dataEntries);
+
+        anyChartView.setZoomEnabled(true);
+        anyChartView.setChart(pie);
+    }
+
 }
